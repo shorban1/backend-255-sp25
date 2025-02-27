89ccb18ca7171c4750a334e515cfc72d84cf9b1f
shorban1 <shorban1@ivytech.edu>
2025-02-27 12:08:34 -0500

    Authentication Lab Changes

--- app.js
+++ app.js
@@ -1,14 +1,72 @@
-//setup
 const express = require("express");
-var cors = require("cors");
-//activate
-const bodyParser = require("body-parser");
 const Song = require("./models/songs");
+var cors = require("cors");
+//const bodyParser = require("body-parser");
+const jwt = require("jwt-simple");
+const User = require("./models/users");
+
 const app = express();
 app.use(cors());
 
 app.use(express.json());
+
 const router = express.Router();
+const secret = "supersecret";
+
+//creating a new user
+router.post("/users", async (req, res) => {
+  if (!req.body.username || !req.body.password) {
+    res.status(400).send("Missing username or password");
+  }
+
+  const newUser = await new User({
+    username: req.body.username,
+    password: req.body.password,
+    status: req.body.status,
+  });
+  try {
+    await newUser.save();
+    console.log(newUser);
+    res.status(201).send("User created");
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+router.post("/auth", async (req, res) => {
+  if (!req.body.username || !req.body.password) {
+    res.status(400).send("Missing username or password");
+    return;
+  }
+  let user = await User.findOne({ username: req.body.username });
+  if (!user) {
+    res.status(401).json({ error: "Bad Username" });
+  } else if (user.password != req.body.password) {
+    res.status(401).send("Bad password");
+  } else {
+    username2 = user.username;
+    const token = jwt.encode({ username: user.username }, secret);
+    const auth = 1;
+
+    res.json({ username2, token: token, auth: auth });
+  }
+});
+
+router.get("/users", async (req, res) => {
+  if (!req.headers["x-auth"]) {
+    return res.status(401).json({ error: "Missing X-Auth" });
+  }
+
+  const token = req.headers["x-auth"];
+  try {
+    const decoded = jwt.decode(token, secret);
+
+    let users = User.find({}, "username status");
+    res.json(users);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
 //get all songs
 router.get("/songs", async (req, res) => {
@@ -49,6 +107,15 @@ router.put("/songs/:id", async (req, res) => {
     res.sendStatus(204);
   } catch (err) {
     res.status(400).send(err);
+  }
+});
+
+router.delete("/songs/:id", async (req, res) => {
+  try {
+    await Song.deleteOne({ _id: req.params.id });
+    res.sendStatus(204);
+  } catch (err) {
+    res.status(400);
   }
 });
 //all requests that usually use an api start with /api
